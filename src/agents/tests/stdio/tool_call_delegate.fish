ca1e75aebbe8a9ecfbd032a35754e12a47e2bc02
Wes Higbee <wes.mcclure@gmail.com>
2026-05-26 07:27:10 -0500

    example

--- src/agents/tests/stdio/tool_call_delegate.fish
+++ src/agents/tests/stdio/tool_call_delegate.fish
@@ -5,7 +5,7 @@ set request_init '{"jsonrpc":"2.0","id":1,"method":"initialize","params":{"proto
 # initialized notification DOES NOT HAVE ID
 set notify_initialized '{"jsonrpc":"2.0","method":"notifications/initialized"}'
 
-set request_call_delegate '{ "jsonrpc": "2.0", "id": 2, "method":"tools/call","params":{"name":"delegate","arguments":{}}}'
+set request_call_delegate '{ "jsonrpc": "2.0", "id": 2, "method":"tools/call","params":{"name":"delegate","arguments":{"description": "what time is it?"}}}'
 
 begin
     echo $request_init
